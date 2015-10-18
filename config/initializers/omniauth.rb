ba07f68a530ccefa525710be74b75d92ffb2c7fc
Ram Iyer <ramky.iyer@yahoo.com>
2015-10-18 12:32:42 -0500

    fix to get facebook auth going

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ FB_APP_ID = ENV['FB_APP_ID']
 FB_APP_SECRET = ENV['FB_APP_SECRET']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FB_APP_ID, FB_APP_SECRET
+  provider :facebook, FB_APP_ID, FB_APP_SECRET, {:provider_ignores_state => true}
 end

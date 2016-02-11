d75ff8fa457d3e57f34a02d79026e5f66abebc5b
Ram Iyer <ramky.iyer@yahoo.com>
2016-02-10 20:07:35 -0600

    adding topics controller specs and removing unnecessary topic_id requirement

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -67,6 +67,6 @@ class TopicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def topic_params
-      params.require(:topic).permit(:name, :description, :type_id)
+      params.require(:topic).permit(:name, :description)
     end
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -26,23 +26,94 @@ RSpec.describe TopicsController, type: :controller do
 
   describe 'POST :create' do
     context 'with correct authentication credentials' do
-      it 'should create a topic with the values provided' do
-        name        = 'My Pet topic'
-        description = 'All things about my pet topic'
-        ram         = create(:account)
-        type        = create(:type)
-        set_current_account(ram)
+      context 'with correct input values' do
+        it 'should create a topic with the values provided' do
+          name        = 'My Pet topic'
+          description = 'All things about my pet topic'
+          ram         = create(:account)
+          set_current_account(ram)
 
-        post :create, topic: { name: name, description: description, types: [type ]}
+          post :create, topic: { name: name, description: description }
 
-        expect(assigns(:topic).name).to eq name
-        expect(assigns(:topic).description).to eq description
+          expect(assigns(:topic).name).to eq name
+          expect(assigns(:topic).description).to eq description
+        end
+      end
+
+      context 'with incorrect input values' do
+        it 'should create a topic with the values provided' do
+          description = 'All things about my pet topic'
+          ram         = create(:account)
+          type        = create(:type)
+          set_current_account(ram)
+
+          post :create, topic: { description: description }
+
+          expect(response).to render_template :new
+        end
       end
     end
 
     context 'without authentication' do
       it_behaves_like 'requires sign in' do
         let(:action) { post :create }
+      end
+    end
+  end
+
+  describe 'PUT :update' do
+    context 'with correct authentication credentials' do
+      context 'with correct input values' do
+        it 'should update a topic with the values provided' do
+          name        = 'My Pet topic'
+          description = 'All things about my pet topic'
+          ram         = create(:account)
+          set_current_account(ram)
+          topic       = create(:topic)
+
+          put :update, id: topic.id, topic: { name: name, description: description }
+
+          expect(assigns(:topic).name).to eq name
+          expect(assigns(:topic).description).to eq description
+        end
+      end
+
+      context 'with incorrect input values' do
+        it 'should create a topic with the values provided' do
+          ram         = create(:account)
+          set_current_account(ram)
+          topic       = create(:topic)
+
+          put :update, id: topic.id, topic: { name: '' }
+
+          expect(response).to render_template :edit
+        end
+      end
+    end
+
+    context 'without authentication' do
+      it_behaves_like 'requires sign in' do
+        let(:action) { put :update, id: create(:topic).id }
+      end
+    end
+  end
+
+  describe 'DELETE :destroy' do
+    context 'with correct authentication credentials' do
+      it 'should destroy the topic' do
+        ram         = create(:account)
+        set_current_account(ram)
+        topic       = create(:topic)
+
+        delete :destroy, id: topic.id
+
+        expect(Topic.count).to eq 0
+      end
+    end
+
+    context 'without authentication' do
+      it_behaves_like 'requires sign in' do
+        let(:action) { delete :destroy, id: create(:topic).id }
       end
     end
   end

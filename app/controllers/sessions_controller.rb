6743c8abe2c2909ecccb02ff5ed035cf03782b03
Ram Iyer <ramky.iyer@gmail.com>
2019-03-19 13:45:10 -0400

    custom authentication with sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    account = Account.from_omniauth(env["omniauth.auth"])
-    if account
+    account = Account.find_by(email_address: params[:login][:email_address])
+    if account && account.authenticate(params[:login][:password])
       session[:account_id] = account.id
-      redirect_to root_url
+      redirect_to root_url, notice: "Logged in!"
     else
       flash[:danger] = 'You are not authorized to view this application.'
       redirect_to sign_in_path

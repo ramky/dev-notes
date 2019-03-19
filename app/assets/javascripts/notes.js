06370cfe611e3ada8e2e9c2b90f0976ebe44c145
Ram Iyer <ramky.iyer@gmail.com>
2019-03-18 23:06:59 -0400

    new clipboard

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -16,12 +16,29 @@ ready = function(){
 
   hljs.initHighlightingOnLoad();
 
+  $('#copy-trigger').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+  });
+
+  function setTooltip(btn, message) {
+    $(btn).tooltip('show')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  }
+
+  function hideTooltip(btn) {
+    setTimeout(function() {
+      $(btn).tooltip('hide');
+    }, 500);
+  }
+
   // Copy clipboard stuff
-  ZeroClipboard.config( { swfPath: "//cdn.jsdelivr.net/zeroclipboard/2.2.0/ZeroClipboard.swf" });
-  var clip = new ZeroClipboard($("#copy-trigger"));
-  clip.on('aftercopy', function(e){
-    $(e.target).after('<div class="alert alert-success">Copied to Clipboard!</div>');
-    $(e.target).next().delay(500).fadeOut();
+  var clipboard = new Clipboard('#copy-trigger');
+
+  clipboard.on('success', function(e) {
+    setTooltip(e.trigger, 'Copied to Clipboard!');
+    hideTooltip(e.trigger);
   });
 };
 

8d0ff367ad4813769ef809e608ca62b1571a0ed1
Ram Iyer <ramky.iyer@gmail.com>
2019-03-19 13:42:40 -0400

    adding new routes to login users per credentials

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   end
 
   resources :types
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  # match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'sign_out', to: 'sessions#destroy', as: 'sign_out', via: [:get, :post]
   match 'sign_in', to: 'sessions#new', as: 'sign_in', via: [:get]
 
